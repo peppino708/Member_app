f8a56f70bbc0b87e7157dce51843d1f75150e351
github peppino708 <708.baseball.ster@gmail.com>
2022-03-01 21:48:37 +0900

    Add tweet_array

--- src/components/pages/Setting.tsx
+++ src/components/pages/Setting.tsx
@@ -1,6 +1,15 @@
 import { Box, Flex, Stack, Text } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
+const textArr = [
+  "Membr.comの移行作業が大変だな〜",
+  "大学4年生が辞めちゃうの寂しいな",
+  "素晴らしいバイトです",
+  "バイトの倍率めちゃくちゃ高い",
+  "新スタッフどんな子か早くみたい",
+  "今日は1時間残業しました",
+];
+
 export const Setting: VFC = memo(() => {
   return (
     <Flex justify={"center"} align={"center"} height={"100vh"}>
@@ -17,15 +26,13 @@ export const Setting: VFC = memo(() => {
           <Text textAlign={"center"} fontSize="5xl" fontWeight="bold" mb={4}>
             Tweet
           </Text>
-          <Text fontSize="2xl" color="gray">
-            Membr.comの移行作業が大変だな〜
-          </Text>
-          <Text fontSize="2xl" color="gray">
-            大学4年生が辞めちゃうの寂しいな
-          </Text>
-          <Text fontSize="2xl" color="gray">
-            素晴らしいバイトです
-          </Text>
+          {textArr.map((text) => {
+            return (
+              <Text fontSize="2xl" color="gray" shadow={"md"} p={2} m={2}>
+                {text}
+              </Text>
+            );
+          })}
         </Stack>
       </Box>
     </Flex>
