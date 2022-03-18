4fe4cefcff4d6f04b778c7e633ff3b1ea40845d8
github peppino708 <708.baseball.ster@gmail.com>
2022-03-18 13:26:29 +0900

    Change from user to member

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,5 @@ class ApplicationController < ActionController::Base
         include DeviseTokenAuth::Concerns::SetUserByToken
       
         skip_before_action :verify_authenticity_token
-        helper_method :current_user, :user_signed_in?
+        helper_method :current_member, :member_signed_in?
       end
