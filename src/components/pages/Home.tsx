ab68aeda45217d35f93c16cb67c37368febc1319
github peppino708 <708.baseball.ster@gmail.com>
2022-03-20 13:11:01 +0900

    Styling Home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Image } from "@chakra-ui/react";
+import { Box, Image, Stack, Text } from "@chakra-ui/react";
 import React, { useContext } from "react";
 
 import { AuthContext } from "../../router/Router";
@@ -11,18 +11,43 @@ export const Home: React.FC = () => {
     <>
       {isSignedIn && currentUser ? (
         <>
-          <Image
-            borderRadius="full"
-            boxSize="160px"
-            src={
-              currentUser.image.url
-                ? currentUser.image.url
-                : "https://res.cloudinary.com/dfw3mlaic/image/upload/v1/images/unknown_ffqtxf"
-            }
-            m="auto"
-          />
-          <h2>Email: {currentUser?.email}</h2>
-          <h2>Name: {currentUser?.name}</h2>
+          <Box
+            w={{ base: "260px", md: "500px" }}
+            h={{ base: "260px", md: "500px" }}
+            bg="white"
+            borderRadius="10px"
+            shadow="md"
+            p={4}
+            overflow={"auto"}
+          >
+            <Image
+              borderRadius="full"
+              boxSize={{ base: "160px", md: "250px" }}
+              src={
+                currentUser.image.url
+                  ? currentUser.image.url
+                  : "https://res.cloudinary.com/dfw3mlaic/image/upload/v1/images/unknown_ffqtxf"
+              }
+              m="auto"
+            />
+            <Stack textAlign={"center"} mt={3}>
+              <Text fontSize="lg" color="gray">
+                Email: {currentUser?.email}
+              </Text>
+              <Text fontSize="lg" color="gray">
+                Name: {currentUser?.name}
+              </Text>
+              <Text fontSize="lg" color="gray">
+                nickname: {currentUser?.nickname}
+              </Text>
+              <Text fontSize="lg" color="gray">
+                hobbies: {currentUser?.hobbies}
+              </Text>
+              <Text fontSize="lg" color="gray">
+                recentTopic: {currentUser?.recent_topic}
+              </Text>
+            </Stack>
+          </Box>
         </>
       ) : (
         <h1>Not signed in</h1>
