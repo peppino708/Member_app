70237d4832a05a5fa4ec0c5767457b4f7e26d38e
github peppino708 <708.baseball.ster@gmail.com>
2022-03-27 21:49:17 +0900

    Remove unnecessary has_many and belogs_to

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Member < ActiveRecord::Base
-  # has_many :posts, dependent: :destroy
   mount_uploader :image, ImageUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
