b6b38adef6aebb3b458300fee62281613f5b3204
github peppino708 <708.baseball.ster@gmail.com>
2022-03-27 16:31:14 +0900

    Add TweetInput to Tweet_component

--- src/components/organism/tweet/TweetInput.tsx
+++ src/components/organism/tweet/TweetInput.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import { Box, Button, TextField, Theme } from "@material-ui/core";
+import { Send } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/styles";
+import React, { useState } from "react";
+
+const useStyle = makeStyles((theme: Theme) => ({
+  textField: {
+    backgroundColor: "white",
+  },
+}));
 
 export const TweetInput = () => {
-  return <div>TweetInput</div>;
+  const classes = useStyle();
+  const [tweetMsg, setTweetMsg] = useState("");
+
+  const sendTweet = () => {
+    setTweetMsg("");
+  };
+
+  return (
+    <>
+      <form onSubmit={sendTweet}>
+        <Box display={"flex"}>
+          <TextField
+            style={{ marginRight: 20 }}
+            variant="outlined"
+            fullWidth
+            label="What's happing?"
+            className={classes.textField}
+            value={tweetMsg}
+            onChange={(e) => setTweetMsg(e.target.value)}
+          />
+          <Button
+            type="submit"
+            disabled={!tweetMsg}
+            style={{ borderRadius: "100%" }}
+            variant="contained"
+            color="primary"
+          >
+            <Send />
+          </Button>
+        </Box>
+      </form>
+    </>
+  );
 };
