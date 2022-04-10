8a4f2a104d3057f11c17bf628c82eec0a93fa14e
peppino708 <708.baseball.ster@gmail.com>
2022-04-10 18:40:22 +0900

    Add gem pg and Create .env

--- frontend/src/lib/api/client.ts
+++ frontend/src/lib/api/client.ts
@@ -12,7 +12,7 @@ const options = {
 
 const client = applyCaseMiddleware(
   axios.create({
-    baseURL: "http://localhost:3000/api/v1",
+    baseURL: `${process.env.REACT_APP_SERVER_URL}/api/v1`,
   }),
   options
 );
