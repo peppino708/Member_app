def11c11587605a3e59ebe491f2f02a6fc534971
peppino708 <708.baseball.ster@gmail.com>
2022-04-11 20:52:37 +0900

    Add production_host and dev_host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,11 @@
 CarrierWave.configure do |config|
-  config.asset_host = "https://murmuring-earth-47067.herokuapp.com"
-  config.storage = :file
-  config.cache_storage = :file
+  if Rails.env.production?
+    config.asset_host = "https://murmuring-earth-47067.herokuapp.com"
+    config.storage = :file
+    config.cache_storage = :file
+  else
+    config.asset_host = "http://localhost:3000"
+    config.storage = :file
+    config.cache_storage = :file
+  end
 end
