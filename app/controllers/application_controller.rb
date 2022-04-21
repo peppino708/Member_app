624d1780ec65eeb74b6bb7e24af26e51f69501b3
peppino708 <708.baseball.ster@gmail.com>
2022-04-21 14:23:54 +0900

    Add fallback_index_html_method to function ReactRouter on heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
       
         skip_before_action :verify_authenticity_token
         helper_method :current_member, :member_signed_in?
+
+        def fallback_index_html
+          render :file => 'public/index.html'
+        end
       end
