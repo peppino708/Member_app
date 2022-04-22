dee0adb797a03b0173a3d47bdde3062b75fc9d3c
peppino708 <708.baseball.ster@gmail.com>
2022-04-22 14:37:36 +0900

    本番環境のみS3へ画像アップロード

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,12 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
+  if Rails.env.production?
+    storage :fog # 本番環境のみ
+  else
+    storage :file # 本番環境以外
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-    #本番環境ではS3のオブジェクトURLに設定する
-    config.asset_host = "https://member-app-image.s3.ap-northeast-1.amazonaws.com"
+  if Rails.env.production? # 本番環境:AWS使用
+    config.asset_host = "https://member-app-image.s3.ap-northeast-1.amazonaws.com" #本番環境ではS3のオブジェクトURLに設定する
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = 'member-app-image' # 作成したバケット名を記述
@@ -15,4 +15,9 @@ CarrierWave.configure do |config|
       region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
       path_style: true
     }
+  else
+    config.asset_host = "http://localhost:3000"
+    config.storage :file # 開発環境:public/uploades下に保存
+    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+  end  
 end
